87b0511a10f8288f9622eb53da9e882de2d5c280
Isaiah Odhner <isaiahodhner@gmail.com>
2014-11-05 23:17:56 -0500

    Work locally

--- generator.js
+++ generator.js
@@ -142,9 +142,7 @@ xhr.onerror = function(){
 };
 xhr.onreadystatechange = function(){
 	if(xhr.readyState === 4){
-		if(xhr.status === 200 || xhr.status === 304){
-			generateCards(xhr.responseText);
-		}
+		generateCards(xhr.responseText);
 	}
 	
 };
